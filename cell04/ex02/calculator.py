629a1c00010d4edb0f8c53d690e0d4457c5213c1
wsayamip <wiritpolsayamipuk@gmail.com>
2025-06-22 05:00:22 +0000

    Cell04Ex02fin

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,15 @@
+num1_str = input("Give me the first number: ")
+num1 = float(num1_str)
+num2_str = input("Give me the second number: ")
+num2 = float(num2_str)
+
+addition_result = num1 + num2
+subtraction_result = num1 - num2
+multiplication_result = num1 * num2
+division_result = num1/num2
+
+
+print (num1, "+", num2, "=", addition_result)
+print (num1, "-", num2, "=", subtraction_result)
+print (num1, "*", num2, "=", multiplication_result)
+print (num1, "/", num2, "=", division_result)
