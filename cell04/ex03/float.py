6ec67691c8f126a16662718007e4ff7b9a4e4da2
wsayamip <wiritpolsayamipuk@gmail.com>
2025-06-22 05:13:14 +0000

    cell04Ex03fin

---
+++ cell04/ex03/float.py
@@ -0,0 +1,14 @@
+#cell04ex03 Float
+def classify_and_convert_number():
+    user_input = input("Give me a number: ")
+    try:
+        num_float = float(user_input)
+        if num_float.is_integer():
+            num_int = int(num_float)
+            print(num_int)
+            print("This number is an integer.")
+        else:
+            print("This number is a decimal.")
+    except ValueError:
+        print()
+classify_and_convert_number()
